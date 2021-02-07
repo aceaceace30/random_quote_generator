2a6c1093463468129113aded811e43c6caa0273c
michael.ababao <michaelababao200@gmail.com>
2021-02-07 17:15:46 +0800

    Move debug value to env

--- random_quote_generator/settings.py
+++ random_quote_generator/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG')
 
 ALLOWED_HOSTS = ['127.0.0.1',
                  'aceaceace303030.pythonanywhere.com',
